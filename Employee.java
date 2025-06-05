7f1a9de0159fa92d56b15dcd0fb2d2b48fb4b725
praghnavaidya <praghna9@gmail.com>
2025-06-05 15:41:47 +0530

    saved

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package Program3;
+
+public class Employee {
+
+    private String name;
+    private int id;
+    private double salary;
+    private String department;
+    public Employee(String name, int id, double salary, String department) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.department = department;
+    }
+    public void display() {
+        
+        System.out.println("Details");
+        System.out.println("Employee details of thr company");
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+        System.out.println("Department: " + department);
+    }
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 12345, 50000.0, "Engineering");
+        emp.display();
+    }
+}
+
